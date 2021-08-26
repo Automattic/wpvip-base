e55bf05e93f724cfed7555642917f5b4e76a2b55
Nick Daugherty <nick.daugherty@automattic.com>
2021-08-26 13:39:00 -0600

    Fix relative path

--- src/WPVIP.php
+++ src/WPVIP.php
@@ -6,6 +6,6 @@ class WPVIP {
 	public static function init() {
 		Logging\Error_Logging::init( new Logging\GCP_Stderr_Logger() );
 
-		require_once './advanced-post-cache/advanced-post-cache.php';
+		require_once __DIR__ . '/advanced-post-cache/advanced-post-cache.php';
 	}
 }
