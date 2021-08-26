77f170167bdb4541f4ccd2df5f7eb13255e5dc89
Nick Daugherty <nick.daugherty@automattic.com>
2021-08-26 13:29:50 -0600

    Add advanced post cache

--- src/WPVIP.php
+++ src/WPVIP.php
@@ -5,5 +5,7 @@ namespace Automattic\WPVIP;
 class WPVIP {
 	public static function init() {
 		Logging\Error_Logging::init( new Logging\GCP_Stderr_Logger() );
+
+		require_once './advanced-post-cache/advanced-post-cache.php';
 	}
 }
