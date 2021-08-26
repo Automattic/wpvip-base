630a23f36a0401156f11c0c7ded7ae920fbadeb9
Nick Daugherty <nick.daugherty@automattic.com>
2021-08-26 13:47:38 -0600

    Add stats helpers

--- src/WPVIP.php
+++ src/WPVIP.php
@@ -7,5 +7,6 @@ class WPVIP {
 		Logging\Error_Logging::init( new Logging\GCP_Stderr_Logger() );
 
 		require_once __DIR__ . '/advanced-post-cache/advanced-post-cache.php';
+		require_once __DIR__ . '/stats.php';
 	}
 }
