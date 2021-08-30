12afd5b78984f48f97791e821a749e075fa92186
Nick Daugherty <nick.daugherty@automattic.com>
2021-08-30 09:05:18 -0600

    Add mail dependency

--- src/WPVIP.php
+++ src/WPVIP.php
@@ -5,6 +5,7 @@ namespace Automattic\WPVIP;
 class WPVIP {
 	public static function init() {
 		Logging\Error_Logging::init( new Logging\GCP_Stderr_Logger() );
+		Mail\Mail::init();
 
 		require_once __DIR__ . '/advanced-post-cache/advanced-post-cache.php';
 		require_once __DIR__ . '/stats.php';
